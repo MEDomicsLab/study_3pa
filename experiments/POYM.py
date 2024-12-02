f8f36f89db9d528894e5d54072e7cef4150ee5c9
Olivier998 <olivier.lefebvre3@usherbrooke.ca>
2024-12-02 11:22:44 -0500

    Added 3pa to experiment

--- experiments/POYM.py
+++ experiments/POYM.py
@@ -117,24 +117,24 @@ def poym_experiment():
                            column_labels=test_data['columns'])
 
     # Execute the MED3PA experiment
-    # print(f"Starting Med3PA experiment :{datetime.now()}")
-    # results = Med3paExperiment.run(
-    #     datasets_manager=datasets,
-    #     base_model_manager=base_model_manager,
-    #     uncertainty_metric="sigmoidal_error",
-    #     ipc_type='EnsembleRandomForestRegressor',
-    #     ipc_params=ipc_params,
-    #     apc_params=apc_params,
-    #     ipc_grid_params=ipc_grid,
-    #     apc_grid_params=apc_grid,
-    #     samples_ratio_min=0,
-    #     samples_ratio_max=10,
-    #     samples_ratio_step=5,
-    #     evaluate_models=True,
-    # )
-    #
-    # # Save the results to a specified directory
-    # results.save(file_path=f'experiments/results/poym')
+    print(f"Starting Med3PA experiment :{datetime.now()}")
+    med3pa_results = Med3paExperiment.run(
+        datasets_manager=datasets,
+        base_model_manager=base_model_manager,
+        uncertainty_metric="sigmoidal_error",
+        ipc_type='EnsembleRandomForestRegressor',
+        ipc_params=ipc_params,
+        apc_params=apc_params,
+        ipc_grid_params=ipc_grid,
+        apc_grid_params=apc_grid,
+        samples_ratio_min=0,
+        samples_ratio_max=10,
+        samples_ratio_step=5,
+        evaluate_models=True,
+    )
+
+    # Save the results to a specified directory
+    med3pa_results.save(file_path=f'experiments/results/poym')
 
     # Execute the Med3pa experiment with Detectron results
     med3pa_detectron_results = Med3paDetectronExperiment.run(
@@ -157,6 +157,7 @@ def poym_experiment():
         samples_ratio_max=10,
         samples_ratio_step=5,
         evaluate_models=True,
+        prev_med3pa_results=med3pa_results
     )
 
     # Save the results to a specified directory
